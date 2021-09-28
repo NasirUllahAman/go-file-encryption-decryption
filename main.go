8ad6981de0533bebf79b73de9361eb2a1462dd75
NasirUllahAman <nasirullahaman@gmail.com>
2021-09-28 19:42:16 +0500

    new code

--- main.go
+++ main.go
@@ -4,27 +4,31 @@ import (
 	//"bufio"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 
-	c1 "github.com/NasirUllahAman/go-file-encryption-decryption/pkg"
+	"github.com/NasirUllahAman/go-file-encryption-decryption/pkg"
 )
 
 func main() {
+	key := "passphrasewhichneedstobe32bytes!"
 	data, err := ioutil.ReadFile("test.txt")
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	c := c1.NewCaesar(1)
 	choice := os.Args
 	if choice[1] == "y" {
-		encryptedText := c.Encryption(string(data))
+		encryptedText, err := pkg.Encrypt([]byte(key), string(data))
+		if err != nil {
+			log.Fatal(err)
+		}
 		fmt.Println("Encrypted text", encryptedText)
 
 		ioutil.WriteFile("test.txt", []byte(encryptedText), 0644)
 	} else if choice[1] == "n" {
 
-		decryptedText := c.Decryption(string(data))
+		decryptedText, _ := pkg.Decrypt([]byte(key), string(data))
 
 		ioutil.WriteFile("test.txt", []byte(decryptedText), 0644)
 
